3ecc41add3625d01c856b403c330e30f159d54c6
Madison Simpson <madison_simpson@outlook.com>
2024-03-11 22:41:51 -0400

    Maxed out newspaper pulls

--- main.py
+++ main.py
@@ -1,19 +1,64 @@
-STOCK_NAME = "TSLA"
-COMPANY_NAME = "Tesla Inc"
+import requests
+import datetime
+
+from datetime import datetime
+STOCK_NAME = "XRP"
+COMPANY_NAME = "Ripple Labs"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-    ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
+STOCK_API_KEY = "B2XBZKCX8EGHTHN6"
+NEWS_API_KEY = "1bddf2fcb4b748c0a15085a642301f94"
+
+
+# STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
 #TODO 1. - Get yesterday's closing stock price. Hint: You can perform list comprehensions on Python dictionaries. e.g. [new_value for (key, value) in dictionary.items()]
+stock_params = {
+    "function": "DIGITAL_CURRENCY_DAILY",
+    "symbol": STOCK_NAME,
+    "market": "CAD",
+    "apikey": STOCK_API_KEY
+}
 
-#TODO 2. - Get the day before yesterday's closing stock price
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Digital Currency Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data['4a. close (CAD)']
+print(yesterday_closing_price)
 
-#TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data['4a. close (CAD)']
+print(day_before_yesterday_closing_price)
 
-#TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+print(difference)
+
+diff_percent = (difference / float(yesterday_closing_price)) * 100
+print(diff_percent)
+
+#TODO 1 -> 5
+if diff_percent > 1:
+    print("get news")
+
+news_params = {
+    "q":"XRP",
+    "from":datetime.date.today()- datetime.timedelta(days=2),
+    "to": datetime.date.today()- datetime.timedelta(days=1),
+    "language":"en",
+    "sortBy":"relevancy",
+    "pageSize":3,
+    "apikey": "1bddf2fcb4b748c0a15085a642301f94"
+}
+
+response = requests.get(NEWS_ENDPOINT, params=news_params)
+print(response.json()["articles"])
+"""
+
+
 
 #TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
 
@@ -35,7 +80,7 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 
 #Optional TODO: Format the message like this: 
-"""
+
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
